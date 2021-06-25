b5f6c0f26e0e1f1d807ef0a4301975c2d729e968
Carlos <carlos@falconmasters.com>
2021-06-25 18:22:20 -0500

    Agregando README

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {DatePicker, TimePicker, DateTimePicker} from '@material-ui/pickers';
 
 const App = () => {
+	const [fechaSeleccionada, cambiarFechaSelecionada] = useState(new Date());
+
+	// console.log(fechaSeleccionada)
+
 	return (
 		<div className="contenedor">
 			<div className="grupo">
-				<h1>Hola Mundo</h1>
+				<label>Fecha</label>
+				<DatePicker value={fechaSeleccionada} onChange={cambiarFechaSelecionada} />
+			</div>
+
+			<div className="grupo">
+				<label>Hora</label>
+				<TimePicker value={fechaSeleccionada} onChange={cambiarFechaSelecionada} />
+			</div>
+
+			<div className="grupo">
+				<label>Fecha y Hora</label>
+				<DateTimePicker value={fechaSeleccionada} onChange={cambiarFechaSelecionada} />
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import esLocale from 'date-fns/locale/es';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
+			<App />
+		</MuiPickersUtilsProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
